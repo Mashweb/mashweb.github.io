c8a17e4429bb37ab815825020216a5c2199d6908
Tom Elam <tomelam@gmail.com>
2016-10-16 01:15:18 +0530

    Little big bang integration: combined the best of the NodeLists directory and the layouts directory. Too much redundancy.
    There is a bug in mover.js (in both js directories) that causes a node
    to be lost sometimes. I haven't determined what conditions make that happen.

--- experiments/NodeLists/js/mover.js
+++ experiments/NodeLists/js/mover.js
@@ -1,68 +1,120 @@
 /*
- * Move the first DIV with class "box" from one position to another within
- * its containing NodeList. We want to give it a direct-manipulation feel,
+ * Move any block element contained by an element of CSS class "parent-box"
+ * from one position to another within the parent box's NodeList.
+ * We want to give it a direct-manipulation feel,
  * so we "grab" the DIV by temporarily converting its position value to
  * relative.
  *
  * FIXME: Allow any static block to be moved to any position in the NodeList.
- * Make the setting of the body's margin property to 0, which is being done
- * here in block.html, unnecessary.
+ *
+ * FIXME: Allow any parent element to participate. The parent element is the
+ * element whose children are the moving element or its siblings.
+ * This code is for a prototype GUI, so it is assumed that the parent
+ * element will be determined by a different method than the method used here.
+ *
+ * FIXME: Make the setting of the body's margin property to 0, which is being
+ * done here in block.html, unnecessary.
  */
 
-var boxes, box, body, boxClass, startY, endX, boxTop, parent;
+var boxes, box, body, boxClass, boxTop, startY, endX, boxTop, parent;
 var inDragProcess = false;
-var minGesture = 10; // Moves less than this many pixels don't move in the NodeList.
+
+// Moves smaller than minGesture pixels don't move an element in the NodeList.
+var minGesture = 10;
+
+var boundingRect, boxi, boxInMotion, deltaY;
+var targetBoxIndex = null;
+var criticalPositions = [];
 
 init = function() {
-    boxes = document.getElementsByClassName("box");
-    box = boxes[0];
-    boxTop = box.style.top;
-    boxClass = box.className;
-    box.addEventListener("mousedown", handleMousedown);
-    parent = box.parentElement;
+    var str;
+    initHighlighter();
+    parent = document.getElementsByClassName("parent-box")[0];
+    calcCriticalPositions();
+    parent.addEventListener("mousedown", handleMousedown);
     body = document.getElementsByTagName("body")[0];
     body.addEventListener("mouseup", handleMouseup);
     body.addEventListener("mousemove", handleMousemove);
 }
 
+calcCriticalPositions = function() {
+    boxes = parent.children;
+    totalHeight = 0;
+    str = "critical positions => ";
+    for (boxi = 0; boxi < boxes.length; boxi++) {
+	boundingRect = boxes[boxi].getBoundingClientRect();
+	console.debug("boundingRect.top => " + boundingRect.top +
+		      ", boundingRect.bottom => " + boundingRect.bottom);
+	criticalPositions[boxi] =
+	    Math.round(((boundingRect.bottom - boundingRect.top) * 0.5) + boundingRect.top);
+	str += criticalPositions[boxi] + ", ";
+    }
+    console.debug(str);
+}    
+
 insertAfter = function(newElement, targetElement) {
-    var parent = targetElement.parentNode;
+    var parent = targetElement.parentElement; //CHANGED. WAS parentNode.
     if (parent.lastchild == targetElement) {
 	parent.appendChild(newElement);
     } else {
+	//console.debug("newElement => " + newElement + ", targetElement => " + targetElement);
 	parent.insertBefore(newElement, targetElement.nextSibling);
     }
+    //calcCriticalPositions();
+    targetBoxIndex = null;
 }
 
 handleMousedown = function(event) {
-    console.debug("mousedown, clientX=" + event.clientX + ", clientY=" +
-		  event.clientY);
+    console.debug("mousedown, clientY=" + event.clientY);
+    boxInMotion = event.target;
+    boxTop = boundingRect.top;
+    boxClass = boxInMotion.className;
+    boxTop = boxInMotion.style.top;
+    //console.debug("boxTop => " + boxTop + ", boxBottom => " + boundingRect.bottom + ", boxClass => " + boxClass);
     startY = event.clientY;
-    box.style.position = "relative";
-    box.className += " draggable-block";
+    boxInMotion.style.position = "relative";
+    boxInMotion.className += " draggable-block";
     event.preventDefault();
     inDragProcess = true;
 }
 
 handleMouseup = function(event) {
-    console.debug("mouseup, clientX=" + event.clientX + ", clientY=" +
-		  event.clientY);
+    console.debug("mouseup, clientY=" + event.clientY);
     endY = event.clientY;
     if (endY > startY + 10) {
-	console.debug("Removing box and reinserting after the next box.");
-	parent.removeChild(box);
-	insertAfter(box, boxes[0]);
-	box.className = boxClass;
+	console.debug("endY => " + endY + ", startY => " + startY);
+	if (targetBoxIndex !== null) {
+	    parent.removeChild(boxInMotion);
+	    console.debug("targetBoxIndex => " + targetBoxIndex);
+	    insertAfter(boxInMotion, boxes[targetBoxIndex - 1]); // Subtract 1 because we removed boxInMotion.
+	} else {
+	    console.debug("targetBoxIndex null");
+	}
     }
-    box.style.position = "static";
-    box.style.top = boxTop;
+    boxInMotion.style.position = "static";
+    boxInMotion.className = boxClass;
+    boxInMotion.style.top = boxTop;
+    calcCriticalPositions();
     inDragProcess = false;
 }
 
 handleMousemove = function(event) {
-    console.debug("handleMousemove: event.clientY => " + event.clientY);
     if (inDragProcess) {
-	box.style.top = Math.max(event.clientY - startY, 0);
+	deltaY = event.clientY - startY;
+	boxInMotion.style.top = Math.max(deltaY, 0);
+	for (boxi = 0; boxi < boxes.length; boxi++) {
+	    if (boxes[boxi] != boxInMotion) {
+		boundingRect = boxInMotion.getBoundingClientRect();
+		//console.debug("boxInMotion top => " + boundingRect.top);
+		//console.debug("criticalPositions[boxi] => " +
+		//	      criticalPositions[boxi] + ", Target box index => " + boxi);
+		if (boundingRect.bottom > criticalPositions[boxi]) {
+		    targetBoxIndex = boxi;
+		    //console.debug("boundingRect.top => " + boundingRect.top + ", boxi => " + boxi +
+		    //		  ", criticalPositions[boxi] => " + criticalPositions[boxi]);
+		}
+	    }
+	}
     }
 }
     
---
+++ experiments/NodeLists/js/outliner.js
@@ -0,0 +1,55 @@
+var computedProps   = [];
+
+function outlineOneNode( node ) {
+    var computedStyle;
+    var propTab         = [ "marginTop",     "marginRight",     "marginBottom",     "marginLeft"     ];
+    var computedPropTab = [ "margin-top",    "margin-right",    "margin-bottom",    "margin-left"    ];
+    node.zen = {};
+    node.zen.saveStyles = {};
+    if ( node.style !== undefined ) {
+	node.zen.saveStyles.border = node.style.border;
+	// FIXME: This isn't optimal. It should do something like what the ensureMargin function does, but for borders.
+	node.style.border = "2px solid red";
+	node.zen.saveStyles.display = node.style.display;
+	//node.style.display = "block";
+	node.zen.saveStyles.margin = node.style.margin;
+	// getComputedStyle is necessary here to accomodate any margin-related property in the user-agent stylesheet
+	// such as -webkit-margin-before in Chrome. If such extra margin applied to <h1> elements were not
+	// accomodated, passing the mouse pointer over an <h1> element would cause the margin to shrink suddenly
+	// to just one pixel--a drastic and possibly disconcerting change of appearance.
+	computedStyle = window.getComputedStyle(node, null);
+	for ( propIndex = 0; propIndex < 4; propIndex++ ) {
+	    computedProps[propIndex] = computedStyle.getPropertyValue( computedPropTab[propIndex] );
+	    ensureEnoughMargin( node, propTab[propIndex], computedProps[propIndex] );
+	}
+    }
+}
+
+// This function sets the top, right, bottom, or left of a node to 2 pixels
+// unless the computed margin style is 2 or more pixels.
+// The prop argument should be the property string for just one margin,
+// i.e. of the format "3px", not "0px 3px" or the like.
+// The computedProp argument should be the computed style for just one margin.
+function ensureEnoughMargin( node, prop, computedProp ) {
+    //console.debug("ensureMargin: prop => " + prop + ", computedProp => " + computedProp);
+    if ( computedProp.slice( 0, computedProp.length - 2 ) < 2 ) {
+	//console.debug( "Setting margin" );
+	node.style[prop] = "2px";
+    }
+}
+
+function outlineAllNodes( ) {
+    walkDOM( document.body,
+	     function( node ) {
+		 outlineOneNode( node );
+	     });
+}
+
+function walkDOM( node, func ) {
+    func( node );                     //What does this do?
+    node = node.firstChild;
+    while( node ) {
+        walkDOM( node, func );
+        node = node.nextSibling;
+    }
+};
